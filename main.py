55339eacb8079e55fff951decbc195f0a03fbf21
petak5 <peterurgos12345@gmail.com>
2017-07-20 16:49:32 +0200

    added comments, snake's head picture, need to align snake with the fruit

--- main.py
+++ main.py
@@ -17,26 +17,39 @@ FPS = 15
 screenWidth = 800
 screenHeight = 600
 
-objSize = 10
+fruitSize = 20
+
+objSize = 20
 #objWidth = 10
 #objHeight = 10
 
-sizeOfMove = 10
+sizeOfMove = objSize
 
 gameDisplay = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption('Snek')
+
+img = pygame.image.load("snakeHead.png")
 
 clock = pygame.time.Clock()
 
 font = pygame.font.SysFont(None, 25)
 
 def snake(objSize, snakeList):
-    for XnY in snakeList:
+    gameDisplay.blit(img, (snakeList[-1][0], snakeList[-1][1]))
+    for XnY in snakeList[:-1]:
         pygame.draw.rect(gameDisplay, darkerGreen, [XnY[0], XnY[1], objSize, objSize])
 
+def text_objects(text, color):
+    textSurface = font.render(text, True, color)
+    return textSurface, textSurface.get_rect()
+
 def message_to_screen(message, color):
-    screen_text = font.render(message, True, color)
-    gameDisplay.blit(screen_text, [screenWidth / 2, screenHeight / 2])
+    # screen_text = font.render(message, True, color)
+    # gameDisplay.blit(screen_text, [screenWidth / 2, screenHeight / 2])
+
+    textSurface, textRectangle = text_objects(message, color)
+    textRectangle.center = (screenWidth / 2, screenHeight / 2)
+    gameDisplay.blit(textSurface, textRectangle)
 
 def game_loop():
     gameExit = False
@@ -49,18 +62,30 @@ def game_loop():
     snakeList = []
     snakeLenght = 1
 
-    fruitSize = 10
-
+    # Generating random apple position and making it divisible by 10
     randAppleX = round(random.randrange(0, screenWidth - objSize) / 10.0) * 10.0
     randAppleY = round(random.randrange(0, screenHeight - objSize) / 10.0) * 10.0
 
+
+    # NEED TO FIX !
+
+    # Making the apple divisible by objSize, so they perfectly align
+    if (randAppleX % objSize != 0):
+        randAppleX += (objSize - (randAppleY % objSize))
+
+    if (randAppleY % objSize != 0):
+        randAppleY += (objSize - (randAppleY % objSize))
+
+    # While we don't want to exit game
     while not gameExit:
 
+        # Game over
         while gameOver == True:
             gameDisplay.fill(white)
             message_to_screen("Game over, press C to play again or Q to quit", red)
             pygame.display.update()
 
+            # Events in game over menu
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_q:
@@ -72,11 +97,12 @@ def game_loop():
                     gameOver = False
                     gameExit = True
 
-
+        # Game events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 gameExit = True
 
+            # Movement events
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     lead_x_change = -sizeOfMove
@@ -91,41 +117,60 @@ def game_loop():
                     lead_x_change = 0
                     lead_y_change = sizeOfMove
 
-
-
-
+        # changing x and y position of the new block (head)
         lead_x += lead_x_change
         lead_y += lead_y_change
 
-        if lead_x < 0 or lead_x > screenWidth - objSize or lead_y < 0 or lead_y > screenHeight - objSize:
+        # check of wall collision
+        if lead_x < 0 or lead_x + objSize > screenWidth or lead_y < 0 or lead_y + objSize > screenHeight:
             gameOver = True
 
 
-
+        # wiping board
         gameDisplay.fill(white)
+        
+        # drawing apple
         pygame.draw.rect(gameDisplay, red, [randAppleX, randAppleY, fruitSize, fruitSize])
 
+        # list of head's position
         snakeHead = []
         snakeHead.append(lead_x)
         snakeHead.append(lead_y)
+
+        # appending head to the list of snake body
+        # !! head is located at the end of the list !!
         snakeList.append(snakeHead)
 
+        # deleting last element of snake (first of the list), unless snake eats fruit
         if len(snakeList) > snakeLenght:
             del snakeList[0]
 
-        for eachSegmen in snakeList[: -1]:
-            if eachSegmen == snakeHead:
+        # checking of self collision
+        for eachSegment in snakeList[: -1]:
+            if eachSegment == snakeHead:
                 gameOver = True
 
         snake(objSize, snakeList)
 
         pygame.display.update()
 
+        # collision logic, kinda
         if lead_x == randAppleX and lead_y == randAppleY:
             print("I gotcha")
             randAppleX = round(random.randrange(0, screenWidth - objSize) / 10.0) * 10.0
             randAppleY = round(random.randrange(0, screenHeight - objSize) / 10.0) * 10.0
             snakeLenght += 1
+
+        # # more complex collision logic, which is not working well
+        # # need more work, but we dont need it for this simple snake
+        # if lead_x >= randAppleX and lead_x <= randAppleX + fruitSize or lead_x + objSize >= randAppleX and lead_x + objSize <= randAppleX + fruitSize:
+        #     if lead_y >= randAppleY and lead_y <= randAppleY + fruitSize or lead_y + objSize >= randAppleY and lead_y + objSize <= randAppleY + fruitSize:
+        #         print("I gotcha")
+        #         randAppleX = round(random.randrange(0, screenWidth - objSize) / 10.0) * 10.0
+        #         randAppleY = round(random.randrange(0, screenHeight - objSize) / 10.0) * 10.0
+        #         snakeLenght += 1
+
+
 
         clock.tick(FPS)
 
